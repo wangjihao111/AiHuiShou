17f28fe29027dc8f26a732528b8ab9ad6592a697
wangjihao111 <53158128+wangjihao111@users.noreply.github.com>
2019-12-11 17:52:06 +0800

    第二次上传  配置好proxy

---
+++ src/pages/home/laptopSell/index.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react"
+
+
+class LaptopSell  extends Component{
+    render(){
+        return(
+            <div>
+                LaptopSell
+            </div>
+        )
+    }
+}
+
+export default LaptopSell
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Recommend from "./home/recommend/index"
+import PhoneSell from "./home/phoneSell"
+import LaptoSell from "./mock/laptoSell"
+import TabletSell from "./mock/tabletSell"
+
+
+export {
+    Recommend,
+    PhoneSell,
+    LaptoSell,
+    TabletSell
+}
